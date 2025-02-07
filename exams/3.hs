eff9b8d0718aa2ed6916be0acefe9e1818599938
Eusebiu <eusebiu.rimboi04@gmail.com>
2025-02-07 12:17:22 +0200

    Add another exam model

--- exams/3.hs
+++ exams/3.hs
@@ -1,5 +1,22 @@
 -- link: https://github.com/FMI-Materials/FMI-Materials/blob/main/Year%20II/Semester%20I/Programare%20Functionala/Modele%20Examen/2023%20-%202024/Subiect%2004.jpg
 
+import Data.Char
+import Data.Either
+
+-- Ex 1
+
+transformString :: String -> String
+transformString str = do
+    ch <- str
+    if isLower ch then
+        return ('*')
+    else if isDigit ch then
+        [ch] ++ [ch]
+    else if elem ch "ADT" then
+        []
+    else
+        return (ch)
+
 -- Ex 2
 
 data Pereche a b = MyP a b deriving Show
@@ -16,4 +33,35 @@ instance MyOp Lista where
 lp :: Lista (Pereche Int Char)
 lp = MyL [MyP 97 'a', MyP 3 'b', MyP 100 'd']
 
+-- Ex 3
+
+
+newtype AE a = AE { getAE :: (Either String a, String) } deriving Show
+
+extractRight :: Either a b -> b
+extractRight (Right x) = x
+extractRight (Left _) = error "Tried to extract Right from Left!"
+
+instance Monad AE where
+    return x = AE (Right x, "")
+    AE (Right val, msg1) >>= g =
+        let (result, msg2) = getAE (g val)
+        in AE (result, msg1 ++ msg2)
+
+    AE (Left err, msg) >>= _ = AE (Left err, msg)
+
+instance Applicative AE where
+    pure = return
+    aef <*> aev = do
+        f <- aef
+        f <$> aev
+
+instance Functor AE where
+    fmap f aev = pure f <*> aev
+
+testAE :: AE Int
+testAE = ma >>= f
+    where
+        ma = AE (Right 8, "ana are ")
+        f x = AE (if x `mod` 2 == 0 then Right x else Left "error", "mere si pere")
 
